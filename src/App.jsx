b196f387345b73ee2024d7713fb874916d3b6f0c
tenzijn <tenzinkunchok2022@gmail.com>
2023-12-29 11:36:20 +0100

    added mobile navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './styles/App.css';
+import NavbarMobile from './components/NavbarMobile';
+
+const navLinks = ['Home', 'About', 'Projects', 'Contact'];
 
 function App() {
   return (
     <>
+      <NavbarMobile userName='Tenzin Kunchok' navLinks={navLinks} />
       <h1>Hello world! </h1>
       <h2>This is my portfolio</h2>
     </>
---
+++ src/components/NavbarMobile.jsx
@@ -0,0 +1,64 @@
+import {
+  Flex,
+  Spacer,
+  Text,
+  Accordion,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  AccordionButton,
+  Hide,
+} from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+import '../styles/Navbar.scss';
+import ReactPropTypes from 'prop-types';
+
+const NavbarMobile = (props) => {
+  const { userName, navLinks } = props;
+  return (
+    <Hide above='lg'>
+      <Accordion className='navbar' allowToggle>
+        <AccordionItem>
+          <Flex
+            justifyContent='space-between'
+            alignItems='center'
+            width={'100%'}
+            gap={2}
+          >
+            <Text fontSize='xl' color='white' m={3}>
+              {userName}
+            </Text>
+            <Spacer />
+            <AccordionButton w={20}>
+              <AccordionIcon
+                as={HamburgerIcon}
+                w={12}
+                h={8}
+                color='white'
+                border={'1px solid rgba(255, 255, 255, 0.2)'}
+                borderRadius='md'
+                _hover={{ bg: 'rgba(255, 255, 255, 0.2)' }}
+              />
+            </AccordionButton>
+          </Flex>
+          <AccordionPanel>
+            <Flex flexDirection={'column'} gap={2}>
+              {navLinks.map((link) => (
+                <Text key={link} fontSize='lg' color='white'>
+                  {link}
+                </Text>
+              ))}
+            </Flex>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </Hide>
+  );
+};
+
+NavbarMobile.propTypes = {
+  userName: ReactPropTypes.string.isRequired,
+  navLinks: ReactPropTypes.array.isRequired,
+};
+
+export default NavbarMobile;

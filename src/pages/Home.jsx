d1a6ecf4a2a728ac16c02cc4834d0de41412a058
tenzijn <tenzinkunchok2022@gmail.com>
2024-01-02 19:10:16 +0100

    home minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import { Flex, Grid, GridItem, Heading, Image } from '@chakra-ui/react';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+      <GridItem colSpan={{ base: 3, lg: 1 }}>
+        <Flex alignItems='center' justifyContent='center' h='100%'>
+          <Heading as='h1' size='4xl'>
+            Tenzin
+          </Heading>
+        </Flex>
+      </GridItem>
+      <GridItem colSpan={{ base: 3, lg: 2 }}>
+        <Image src='/tenzin-profile.png' alt='Tenzin Profile' />
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default Home;

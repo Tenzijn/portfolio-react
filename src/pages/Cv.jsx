6c4cbcd542cee595687c31fe50b3fc0a73e23bab
Tenzin Kunchok <145247192+Tenzijn@users.noreply.github.com>
2024-04-15 15:43:01 +0200

    cv page is added

--- src/pages/Cv.jsx
+++ src/pages/Cv.jsx
@@ -1,5 +1,18 @@
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+
 function Cv() {
-  return <div>cv</div>;
+  return (
+    <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
+      <div
+        style={{
+          border: '1px solid rgba(0, 0, 0, 0.3)',
+          height: '750px',
+        }}
+      >
+        <Viewer fileUrl='/TenzinKunchokCV.pdf' />
+      </div>
+    </Worker>
+  );
 }
 
 export default Cv;
